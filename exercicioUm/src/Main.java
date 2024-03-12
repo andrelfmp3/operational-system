81f0ff1fa1da556cd21534328a19430830d705a1
André Lyra <97136270+andrelfmp3@users.noreply.github.com>
2024-03-12 20:41:32 -0300

    update final

--- exercicioUm/src/Main.java
+++ exercicioUm/src/Main.java
@@ -1,21 +1,20 @@
 import java.io.IOException;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
 
-        Scanner teclado = new Scanner(System.in);
-        String appName = teclado.next();
-
-        try {
-            Process process = new ProcessBuilder(appName).start();
-            long pid = process.pid();
-            System.out.println("PID do processo " + appName + ": " + pid);
-        } catch (IOException e) {
-            e.printStackTrace();
+        if (args.length > 0) { //verifica se tem algo no args
+            try {
+                Process process = new ProcessBuilder(args[0]).start();
+                long pid = process.pid();
+                System.out.println("PID do processo " + args[0] + ": " + pid);
+            } catch (IOException e) { //programa não existe
+                System.out.println("Erro");
+            }
+        } else { //args vazio, retorna processo atual
+            long pid = ProcessHandle.current().pid();
+            System.out.println("PID do processo atual: " + pid);
         }
-
-        teclado.close();
-
     }
 }
+
